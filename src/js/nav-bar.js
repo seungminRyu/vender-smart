0b5060ab4e46c142afb879bd0f4a36b837fb98bb
smryu <fbtmdals011@gmail.com>
2020-10-23 17:02:58 +0900

    1023 git init

---
+++ src/js/nav-bar.js
@@ -0,0 +1,6 @@
+const $menuBtn = document.querySelector(".nav__menu-btn");
+const $menu = document.querySelector(".nav-right")
+
+$menuBtn.addEventListener("click", () => {
+    $menu.classList.toggle("active");
+});
