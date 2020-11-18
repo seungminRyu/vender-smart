320d642b7ab9dd32a54ab25e854696fb861a6b5c
seungminRyu <56435483+seungminRyu@users.noreply.github.com>
2020-11-18 13:56:28 +0900

    add 카카오페이 결제 테스트

--- static/js/mukertonAni.js
+++
@@ -1,7 +0,0 @@
-const $mukathonSpan = document.querySelector(".mukathon");
-const $mukathonP = document.querySelector(".mukathon-description");
-
-$mukathonSpan.addEventListener("click", () => {
-    $mukathonSpan.style.animationPlayState = "running";
-    $mukathonP.style.animationPlayState = "running";
-});
